7bf446bd81253b8e2632939dac94902bfc66b59c
Kamil Owczarek <k.owczarek86@gmail.com>
2020-03-25 09:23:30 +0100

    Add account activation

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -5,6 +5,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   def setup
     @admin = users(:michael)
     @non_admin = users(:archer)
+    @unactived_user = users(:lana)
   end
 
   test "index as admin include pagination and delete links" do
@@ -14,9 +15,13 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     assert_select "div.pagination", count: 2
     first_page_of_users = User.paginate(page: 1)
     first_page_of_users.each do |user|
-      assert_select 'a[href=?]', user_path(user), text: user.name
-      unless user == @admin
-        assert_select 'a[href=?]', user_path(user), text: 'delete'
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
       end
     end
     assert_difference 'User.count', -1 do
@@ -29,4 +34,29 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
     get users_path
     assert_select 'a', text: 'delete', count: 0
   end
+
+
+  test "should only show profiles of activated users" do
+    log_in_as(@admin)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    User.paginate(page: 1).each do |user|
+      if user.activated?
+        assert_select 'a[href=?]', user_path(user), text: user.name
+        unless user == @admin
+          assert_select 'a[href=?]', user_path(user), text: 'delete'
+        end
+      else
+        assert_select 'a[href=?]', user_path(user), false
+      end
+    end
+  end
+
+  test "Should not show unactivated user" do
+    log_in_as (@admin)
+    get user_path(@unactived_user)
+    assert_redirected_to root_url
+  end
+
 end
